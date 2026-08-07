99cee518fced59ea191b3874f73bc9400fabe095
shroukshams <shroukshams237@gmail.com>
2026-08-07 20:46:34 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -22,8 +22,8 @@ return [
     "allowed_origins" => [
     "https://vu-platform.vercel.app", 
     "https://vuplatformzikola.duckdns.org", 
-    "http://localhost:3000"
-    #"https://vuplatformzikolaa.duckdns.org"
+    "http://localhost:3000",
+    "https://vuplatformzikolaa.duckdns.org"
 ],
 
     'allowed_origins_patterns' => [],
