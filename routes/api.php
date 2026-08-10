343c8851cb8cc65d9c8b47447553acdf9c8e7309
Ahmed Fekry <a7medfekry24@gmail.com>
2026-08-10 16:00:08 +0300

    resolved

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
+
 Route::get('Home/state', [HomeController::class, 'state']);
 Route::post('Home/subscribers', [HomeController::class, 'subscriber']);
 
